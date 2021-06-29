40cd937d7452ce7de2cf900db1a09fe2b41cbb98
DORJI <dorjitshering370@gmail.com>
2021-06-29 09:21:58 -0800

    rearrangement of social link icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import '../styles/Footer.css';
 import { MdCopyright } from 'react-icons/md';
+import { FaFacebookF }  from 'react-icons/fa';
+import { GrTwitter, GrGithub, GrLinkedinOption } from 'react-icons/gr';
+
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p className="copyright"><MdCopyright className="c-icon" /> 2021 by Dorji Tshering</p>
+		<section className="social-links">
+			<a href="https://www.facebook.com/profile.php?id=100015009334635" 
+			className="social-link"><FaFacebookF className="social-icon facebook" size={14}/></a>
+			<a href="https://twitter.com/DorjiY_Dear" 
+			className="social-link"><GrTwitter className="social-icon twitter" size={14}/></a>
+			<a href="https://www.linkedin.com/in/dorji-tshering-833919206/" 
+			className="social-link"><GrLinkedinOption className="social-icon linkedin" size={14}/></a>
+			<a href="https://github.com/dorji-tshering" 
+			className="social-link"><GrGithub className="social-icon github" size={14}/></a>
+		</section>
+            	<p className="copyright"><MdCopyright className="c-icon" /> 2021 by Dorji Tshering</p>
         </footer>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,6 @@ import '../styles/Header.css';
 import { FcMenu } from 'react-icons/fc';
 import { RiHomeHeartFill } from 'react-icons/ri';
 import { GiSkills } from 'react-icons/gi';
-import { FaFacebookF }  from 'react-icons/fa';
-import { GrTwitter, GrGithub, GrLinkedinOption } from 'react-icons/gr';
 import { MdPersonPin, MdPermPhoneMsg } from 'react-icons/md';
 import { IoCloseSharp } from 'react-icons/io5';
 import { NavLink, Link } from 'react-router-dom';
@@ -31,16 +29,7 @@ const Header = () => {
             <section className="navbar">
                 <Link to="/" className="dorji-tshering"><span className="dorji">D</span>|
                 <span className="tshering">T</span></Link>
-                <section>
-                    <a href="https://www.facebook.com/profile.php?id=100015009334635" 
-                        className="social-link"><FaFacebookF className="social-icon facebook" size={14}/></a>
-                    <a href="https://twitter.com/DorjiY_Dear" 
-                        className="social-link"><GrTwitter className="social-icon twitter" size={14}/></a>
-                    <a href="https://www.linkedin.com/in/dorji-tshering-833919206/" 
-                        className="social-link"><GrLinkedinOption className="social-icon linkedin" size={14}/></a>
-                    <a href="https://github.com/dorji-tshering" 
-                        className="social-link"><GrGithub className="social-icon github" size={14}/></a>
-                </section>
+                
                 { showLink ? <IoCloseSharp onClick={()=> setShowLink(!showLink)} className="main-menu close-icon"/> :                
                 <FcMenu onClick={()=> setShowLink(!showLink)} className="main-menu" />         
             }
